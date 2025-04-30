59c4f9907eea416b605d36f08a31c3279a22ab36
Rosa Kang <therosessom@gmail.com>
2025-04-30 15:00:31 -0400

    update JS

--- src/data.ts
+++ src/data.ts
@@ -93,6 +93,20 @@ export const questions: Question[] = [
     topic: "JavaScript Fundamentals"
   },
   {
+    id: 13,
+    title: "JavaScript Question - Debounce (반동제어)",
+    question: "Given a function fn and a time in milliseconds t, return a debounced version of that function. A debounced function is a function whose execution is delayed by t milliseconds and whose execution is cancelled if it is called again within that window of time. The debounced function should also receive the passed parameters. For example, let's say t = 50ms, and the function was called at 30ms, 60ms, and 100ms. The first 2 function calls would be cancelled, and the 3rd function call would be executed at 150ms. If instead t = 35ms, The 1st call would be cancelled, the 2nd would be executed at 95ms, and the 3rd would be executed at 135ms.",
+    answer: "const debounce = (fn, t) => {\n   let timeoutId;\n   return (...args) => {\n         clearTimeout(timeoutId);\n         timeoutId = setTimeout(() => fn(...args), t);\n    };\n};",
+    topic: "JavaScript Fundamentals"
+  },
+  {
+    id: 14,
+    title: "JavaScript Question - Execute Asynchronous Function",
+    question: "Given an array of asynchronous functions functions, return a new promise promise. Each function in the array accepts no arguments and returns a promise. All the promises should be executed in parallel.",
+    answer: "var promiseAll = function(functions) {\n    let promises = functions.map((fn)=> fn())\n   return Promise.all(promises);\n };",
+    topic: "JavaScript Fundamentals"
+  },
+  {
     id: 1,
     title: "Create a simple Express server",
     question: "Write code to create an Express server that listens on port 3000 and has a single route '/' which returns 'Hello, World!'",
