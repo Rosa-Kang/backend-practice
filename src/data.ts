ad96472bdbd10ba1a31bc0dfbb9bc728b0ea7840
Rosa Kang <therosessom@gmail.com>
2025-05-06 15:04:00 -0400

    update the dt

--- src/data.ts
+++ src/data.ts
@@ -121,6 +121,20 @@ export const questions: Question[] = [
     topic: "JavaScript Fundamentals"
   },
   {
+    id: 17,
+    title: "JavaScript Question - Array Prototype Last",
+    question: "Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1. You may assume the array is the output of JSON.parse.",
+    answer: "Array.prototype.last = function() {\n   return this.length === 0 ? -1 : this[this.length - 1];\n };",
+    topic: "JavaScript Fundamentals"
+  },
+  {
+    id: 18,
+    title: "JavaScript Question - Group By",
+    question: "Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array. A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array which generate that key. The provided callback fn will accept an item in the array and return a string key. The order of each value list should be the order the items appear in the array. Any order of keys is acceptable. Please solve it without lodash's _.groupBy function.",
+    answer: "Array.prototype.groupBy = function(fn) {\n //grouped(array) is an object where each key is the output of fn(arr[i])\n //item is the item inside this array\n  return this.reduce((grouped, item) => {\n     let key = fn(item);\n     if(!grouped[key]) grouped[key] = [];\n     grouped[key].push(item);\n     return grouped;\n  } , {})\n};",
+    topic: "JavaScript Fundamentals"
+  },
+  {
     id: 1,
     title: "Create a simple Express server",
     question: "Write code to create an Express server that listens on port 3000 and has a single route '/' which returns 'Hello, World!'",
