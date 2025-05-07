415ec237df301002bd580c07850ceae94eaf6eb4
Rosa-Kang <49248131+Rosa-Kang@users.noreply.github.com>
2025-05-07 12:18:05 -0400

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -142,6 +142,13 @@ export const questions: Question[] = [
     topic: "JavaScript Fundamentals"
   },
   {
+    id: 20,
+    title: "JavaScript Question - Join Two Arrays By ID",
+    question: "Given two arrays arr1 and arr2, return a new array joinedArray. All the objects in each of the two inputs arrays will contain an id field that has an integer value. joinedArray is an array formed by merging arr1 and arr2 based on their id key. The length of joinedArray should be the length of unique values of id. The returned array should be sorted in ascending order based on the id key. If a given id exists in one array but not the other, the single object with that id should be included in the result array without modification. If two objects share an id, their properties should be merged into a single object: If a key only exists in one object, that single key-value pair should be included in the object. If a key is included in both objects, the value in the object from arr2 should override the value from arr1",
+    answer: "var join = function(arr1, arr2) {\n   const map = new Map();\n\n   for(const obj of arr1) {\n     map.set(obj.id, {...obj});\n  }\n\n   for(const obj of arr2) {\n   if(map.has(obj.id)){\n      map.set(obj.id, {...map.get(obj.id), ...obj})\n   } else {\n      map.set(obj.id, {...obj})\n   }\n }\n  return Array.from(map.values()).sort((a,b) => a.id - b.id)\n};",
+    topic: "JavaScript Fundamentals"
+  },
+  {
     id: 1,
     title: "Create a simple Express server",
     question: "Write code to create an Express server that listens on port 3000 and has a single route '/' which returns 'Hello, World!'",
